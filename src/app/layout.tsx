4a3f767e0799c5d4efbd2354618e74aed400316e
Willy <willy@smartbr.com>
2023-12-13 13:17:29 -0300

    Configurar metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,55 @@
 import { ClientCookiesProvider } from "@components/CookiesProvider";
 import "./globals.css";
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import { Open_Sans } from "next/font/google";
 import { cookies } from "next/headers";
 import LayoutWrapper from "./LayoutWrapper";
-import Head from "next/head";
+
 const sans = Open_Sans({ subsets: ["latin"] });
 
+const APP_NAME = "Recordumm app";
+const APP_DEFAULT_TITLE = "Recordumm app";
+const APP_TITLE_TEMPLATE = "%s - Recordumm app";
+const APP_DESCRIPTION = "Gerencie seus treinos!";
+
 export const metadata: Metadata = {
-  title: "Recordumm app",
-  description: "Gerencie seus treinos",
+  applicationName: APP_NAME,
+  title: {
+    default: APP_DEFAULT_TITLE,
+    template: APP_TITLE_TEMPLATE,
+  },
+  description: APP_DESCRIPTION,
+  manifest: "/manifest.json",
+  appleWebApp: {
+    capable: true,
+    statusBarStyle: "default",
+    title: APP_DEFAULT_TITLE,
+    // startUpImage: [],
+  },
+  formatDetection: {
+    telephone: false,
+  },
+  openGraph: {
+    type: "website",
+    siteName: APP_NAME,
+    title: {
+      default: APP_DEFAULT_TITLE,
+      template: APP_TITLE_TEMPLATE,
+    },
+    description: APP_DESCRIPTION,
+  },
+  twitter: {
+    card: "summary",
+    title: {
+      default: APP_DEFAULT_TITLE,
+      template: APP_TITLE_TEMPLATE,
+    },
+    description: APP_DESCRIPTION,
+  },
+};
+
+export const viewport: Viewport = {
+  themeColor: "#FFFFFF",
 };
 
 export default function RootLayout({
